3ca2047230a48fd810142290ce13fc6156aafb24
Thomas Mansencal <kelsolaar_fool@hotmail.com>
2010-08-06 14:08:54 +0200

    Add "sIBL_GUI_XSI_Server_AssemblyInfo" assembly informations file.

---
+++ Addons/sIBL_GUI_XSI_Server/cssrc/sIBL_GUI_XSI_Server_AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("sIBL_GUI_XSI_Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("sIBL_GUI_XSI_Server")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("98785aeb-5e92-4ed9-b106-af352a70749d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("9.1.89.0")]
+[assembly: AssemblyFileVersion("9.1.89.0")]
