c72aa7fb9f90b535f5b4d0f2117fae40144ca5a4
KelSolaar <thomas.mansencal@gmail.com>
2011-07-30 14:22:34 +0100

    Update utilities.

--- utilities/sIBL_GUI_XSI_Server_gatherFiles.sh
+++ utilities/sIBL_GUI_XSI_Server_gatherFiles.sh
@@ -3,12 +3,12 @@ echo ----------------------------------------------------------------
 echo sIBL_GUI XSI Server - Files Gathering
 echo ----------------------------------------------------------------
 
-#! Gathering Folder Cleanup.
+#! Gathering folder cleanup.
 rm -rf ./releases/repository/*
 
-#! Change Log Gathering.
+#! Change log gathering.
 cp ./releases/Change\ Log.html ./releases/repository/
 
-#! Addon Gathering.
+#! Addon gathering.
 cd ./Addons/
 zip -r ../releases/repository/sIBL_GUI_XSI_Server.zip sIBL_GUI_XSI_Server.xsiaddon
